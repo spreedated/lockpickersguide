1f1055b7a07f5bd3f2fb7b74b32b7357ed49225f
SpReeDateD <webmaster@born2skate.de>
2022-09-14 15:45:30 +0200

    cleanup

--- LockpickersGuide/Models/Options.cs
+++ LockpickersGuide/Models/Options.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Newtonsoft.Json;
 using Npgsql;
-using Newtonsoft.Json;
-using LockpickersGuide.Logic;
-using StackExchange.Redis;
 
 namespace LockpickersGuide.Models
 {

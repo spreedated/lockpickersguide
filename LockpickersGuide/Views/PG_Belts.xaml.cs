4c013a051276302bc0927d1da8e9bfec056d8a63
SpReeDateD <webmaster@born2skate.de>
2022-09-18 18:09:48 +0200

    crud ui

--- LockpickersGuide/Views/PG_Belts.xaml.cs
+++ LockpickersGuide/Views/PG_Belts.xaml.cs
@@ -3,9 +3,18 @@ using LockpickersGuide.Logic;
 using LockpickersGuide.Models;
 using LockpickersGuide.PresentiationModels;
 using Serilog;
+using System;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
 using System.Runtime.CompilerServices;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
 
 namespace LockpickersGuide.Views
 {
@@ -14,7 +23,19 @@ namespace LockpickersGuide.Views
     /// </summary>
     public partial class PG_Belts : LockpickerPage, INotifyPropertyChanged
     {
-        public HashSetLockpicker<Belt> Belts { get; internal set; }
+        public ObservableCollection<Belt> ComboboxBelts { get; internal set; }
+        public ObservableCollection<Belt> Belts { get; internal set; }
+        public ObservableCollection<Belt> DatagridBelts { get; internal set; }
+        public Belt DummyBelt
+        {
+            get
+            {
+                return new()
+                {
+                    Name = "Nothing"
+                };
+            }
+        }
         public PG_Belts()
         {
             InitializeComponent();
@@ -34,10 +55,61 @@ namespace LockpickersGuide.Views
             {
                 Log.Debug($"[View][PG_Brands] First load");
 
-                Belts = ObjectStorage.Belts;
+                this.Belts = new(ObjectStorage.Belts);
                 OnPropertyChanged(nameof(this.Belts));
 
+                this.DatagridBelts = new(ObjectStorage.Belts);
+                OnPropertyChanged(nameof(this.DatagridBelts));
+
+                this.ComboboxBelts = new(ObjectStorage.Belts);
+                this.ComboboxBelts.Insert(0, new() { Name = "Select Belt" });
+                this.ComboboxBelts.Insert(1, new() { Name = "---" });
+                OnPropertyChanged(nameof(this.ComboboxBelts));
+
                 base.firstload ^= true;
+            }
+        }
+
+        private void CMB_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            if (this.CMB_Sortby.SelectedIndex <= 1)
+            {
+                this.DatagridBelts = new(ObjectStorage.Belts);
+                OnPropertyChanged(nameof(this.DatagridBelts));
+                this.CMB_Sortby.SelectedIndex = -1;
+                return;
+            }
+            this.DatagridBelts = new(ObjectStorage.Belts.Where(x => x.Name == ((Belt)CMB_Sortby.SelectedItem).Name));
+            OnPropertyChanged(nameof(this.DatagridBelts));
+        }
+
+        private void BTN_Reset_Click(object sender, RoutedEventArgs e)
+        {
+            this.TXT_Searchtext.Text = null;
+            this.DatagridBelts = new(ObjectStorage.Belts);
+            OnPropertyChanged(nameof(this.DatagridBelts));
+            this.CMB_Sortby.SelectedIndex = -1;
+        }
+
+        private void BTN_Search_Click(object sender, RoutedEventArgs e)
+        {
+            this.DatagridBelts = new(ObjectStorage.Belts.Where(x=>x.Name.ToLower().Contains(TXT_Searchtext.Text.ToLower()) || x.Description.ToLower().Contains(TXT_Searchtext.Text.ToLower())));
+            OnPropertyChanged(nameof(this.DatagridBelts));
+        }
+
+        private void TXT_Searchbox_OnKeyDownHandler(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                this.BTN_Search_Click(this, null);
+            }
+        }
+
+        private void TXT_Searchbox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (((TextBox)sender).Text.Length <= 0)
+            {
+                this.BTN_Reset_Click(this, null);
             }
         }
     }

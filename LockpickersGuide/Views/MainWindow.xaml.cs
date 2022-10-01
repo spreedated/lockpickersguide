3d9cf2e6ba916fc02ad314f914fb5fe3929cae35
SpReeDateD <webmaster@born2skate.de>
2022-10-01 17:14:53 +0200

    removed mainpage

--- LockpickersGuide/Views/MainWindow.xaml.cs
+++ LockpickersGuide/Views/MainWindow.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using LockpickersGuide.Logic;
+using LockpickersGuide.PresentiationModels;
 using LockpickersGuide.ViewLogic;
 using LockpickersGuide.ViewModels;
 using System;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using System.Windows;
@@ -25,7 +28,7 @@ namespace LockpickersGuide.Views
             this.DataContext = new MainWindowViewModel();
             this.Icon = new BitmapImage(new Uri("pack://application:,,,/LockpickersGuide;component/Ressources/icon_white.png"));
             
-            Logic.Preload.PreloadComplete += (o, e) => { this.Dispatcher.Invoke(async () => { await Task.Delay(1500); FRM_Main.Navigate(new Uri("Views\\PG_Main.xaml", UriKind.Relative)); }); };
+            Logic.Preload.PreloadComplete += (o, e) => { this.Dispatcher.Invoke(async () => { await Task.Delay(1500); this.LoadPages(); }); };
 
             ViewModelInstance = (MainWindowViewModel)this.DataContext;
         }
@@ -36,6 +39,38 @@ namespace LockpickersGuide.Views
             {
                 ((Window)sender).DragMove();
             }
+        }
+
+        private void LoadPages()
+        {
+            this.pages.Add(new PG_Brands());
+            this.pages.Add(new PG_CollectionLocks());
+            this.pages.Add(new PG_Belts());
+            this.pages.Add(new PG_Locks());
+
+            FRM_Main.Navigate(this.pages.FirstOrDefault(x=>x.Pagename == "CollectionLocks"));
+        }
+
+        private readonly List<LockpickerPage> pages = new();
+
+        private void BTN_Brand_Click(object sender, RoutedEventArgs e)
+        {
+            this.FRM_Main.Navigate(pages.FirstOrDefault(x => x.Pagename.Equals("brands", StringComparison.InvariantCultureIgnoreCase)));
+        }
+
+        private void BTN_CollectionLocks_Click(object sender, RoutedEventArgs e)
+        {
+            this.FRM_Main.Navigate(pages.FirstOrDefault(x => x.Pagename.Equals("collectionLocks", StringComparison.InvariantCultureIgnoreCase)));
+        }
+
+        private void BTN_Belts_Click(object sender, RoutedEventArgs e)
+        {
+            this.FRM_Main.Navigate(pages.FirstOrDefault(x => x.Pagename.Equals("belts", StringComparison.InvariantCultureIgnoreCase)));
+        }
+
+        private void BTN_Locks_Click(object sender, RoutedEventArgs e)
+        {
+            this.FRM_Main.Navigate(pages.FirstOrDefault(x => x.Pagename.Equals("locks", StringComparison.InvariantCultureIgnoreCase)));
         }
     }
 }
